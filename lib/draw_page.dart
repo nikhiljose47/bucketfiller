f98bf7b59c48f70fce7765331a8a29b1d74543a8
nikhil <nikhiljose47@gmail.com>
2019-10-09 09:56:13 +0530

    Update -  draw paint done

--- lib/draw_page.dart
+++ lib/draw_page.dart
@@ -1,4 +1,3 @@
-import 'package:bucketfiller/bucket_fill.dart';
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
@@ -13,27 +12,50 @@ class DrawingState extends State<Drawing> {
   GlobalKey key = new GlobalKey();
   Stopwatch stopwatch;
   List<Offset> store = [];
+  List<DrawingPoints> points = List();
 
   Color selectedColor = Colors.black;
   Color pickerColor = Colors.black;
   double strokeWidth = 3.0;
-  List<Offset> points = List();
   bool showBottomList = false;
   double opacity = 1.0;
+  StrokeCap strokeCap =  StrokeCap.round;
 
   @override
   Widget build(BuildContext context) {
     return RepaintBoundary(
       key: key,
-      child: GestureDetector(
-        onDoubleTap:()=> _ondtap(context, key, store),
-        
-          onScaleUpdate: (ScaleUpdateDetails update) =>
-              _onScaleUpdate(context, update),
-          onScaleEnd: (ScaleEndDetails end) => _onScaleEnd(context, end),
-       
-        
-       
+      child:GestureDetector(
+        onDoubleTap: ()=>_ondtap(context, key, store),
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  ..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanStart: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  ..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            points.add(null);
+          });
+        },
         child: CustomPaint(
           size: Size.infinite,
           painter: DrawingPainter(
@@ -47,60 +69,37 @@ class DrawingState extends State<Drawing> {
 //pixel Offset(451.0, 390.0)
 //emulator Offset(193.8, 137.5)
   void _ondtap(BuildContext context, key, store) {
-    PainterController painterController =
-        ActivityModel.of(context).painterController;
-    var stopwatch = new Stopwatch()..start();
-    BucketFill().capturePng(key, Offset(193.8, 137.5), store).then((data) {
-      stopwatch.stop();
-      data.add(null);
-      print('stopwatch  ${stopwatch.elapsedMilliseconds}');
-      print('data  $data');
-      print('kolp  ${data.length}');
-      painterController.add(data[0]);
 
-      for (var i = 1; i < data.length; i++) {
-        painterController.updateCurrent(data[i]);
-      }
-    });
-  }
-  void _onScaleStart(BuildContext context, ScaleStartDetails start) {   
-    PainterController painterController =
-        ActivityModel.of(context).painterController;
-    Offset pos = (context.findRenderObject() as RenderBox)
-        .globalToLocal(start.focalPoint);
-    painterController._pathHistory.add(pos);
-    painterController._notifyListeners();
-  }
 
-  void _onScaleUpdate(BuildContext context, ScaleUpdateDetails update) {
-    if (update.scale == 1.0) {
-      Offset pos = (context.findRenderObject() as RenderBox)
-          .globalToLocal(update.focalPoint);
-     //   print('store  $store');
-        this.store = List.from(this.store)..add(pos);
-      } else {
-        
-        this.store = List.from(this.store)..add(pos);
-        print('store  $store');
+    // PainterController painterController =
+    //     ActivityModel.of(context).painterController;
+    // var stopwatch = new Stopwatch()..start();
+    // BucketFill().capturePng(key, Offset(193.8, 137.5), store).then((data) {
+    //   stopwatch.stop();
+    //   data.add(null);
+    //   print('stopwatch  ${stopwatch.elapsedMilliseconds}');
+    //   print('data  $data');
+    //   print('kolp  ${data.length}');
+    //   painterController.add(data[0]);
 
-      }
-    }
+    //   for (var i = 1; i < data.length; i++) {
+    //     painterController.updateCurrent(data[i]);
+    //   }
+    // });
   }
+}
 
-  void _onScaleEnd(BuildContext context, ScaleEndDetails end) {
-    ActivityModel model = ActivityModel.of(context);
-    PathHistory pathHistory = ActivityModel.of(context).pathHistory;
-    PainterController painterController = model.painterController;
-    painterController.endCurrent();
-    model.addDrawing(pathHistory.paths.last); //TODO do this in pathhistory
-    painterController.notifyListeners();
-  }
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
 }
 
 class DrawingPainter extends CustomPainter {
   DrawingPainter({this.pointsList});
   List<DrawingPoints> pointsList;
   List<Offset> offsetPoints = List();
+
   @override
   void paint(Canvas canvas, Size size) {
     for (int i = 0; i < pointsList.length - 1; i++) {
